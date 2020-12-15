43cfef3e23975588ece234b2dde8eaf95ecee57e
Masayuki Kishi <masayukikishi@Masayukis-MacBook-Pro.local>
2020-12-15 22:02:33 +0900

    notes about params updated.

--- scripts/generate_words.py
+++ scripts/generate_words.py
@@ -17,10 +17,10 @@ def create_base_word(word, word_pos, word_tag, word_dep, word_lemma) -> dict:
         Removes non-alphabet characters from beginning and end of word and saves it as lowercase "base_word". (eg. "+High-tech!" → "high-tech" )
     parameters:
         word: str; word to create base_word from
-        word_pos: str?; fill out
-        word_tag: str; fill out
-        word_dep: str; fill out
-        word_lemma: str; fill out
+        word_pos: str?; Coarse-grained part-of-speech from the Universal POS tag set. (eg. noun, verb etc.)
+        word_tag: str; Fine-grained part-of-speech. (eg. NN = singular noun, NNS = plural noun etc.)
+        word_dep: str; Syntactic dependency relation. (What relations the word has to other words in the sentence.)
+        word_lemma: str; Base form of the token, with no inflectional suffixes. (eg. word = changing, lemma = change)
     returns:
         token_dict: dict; a dictionary containing all important parameters of base_word
     """
