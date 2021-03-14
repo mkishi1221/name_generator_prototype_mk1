9b2d72e6830d46db4fd4fa1fe5121e388e24b68b
Korbinian Habereder <haberederkorbinian@googlemail.com>
2021-03-14 16:50:15 +0100

    commit PR changes
      - did some formatting
      - removed unnecessary comments
      - added gitignore entries for python related stuff

---
+++ scripts/modules/find_unique_lines.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import regex as re
+import sys
+import json
+
+def find_unique_lines(text):
+
+    # Replace only-white space lines with nothing, replace multiple spaces with single spaces and split into list
+    text = re.sub(r"^\W+$", "", text)
+    text = re.sub(r"  +", " ", text)
+    lines = text.split("\n")
+
+    lines = [line for line in lines if line != ""]
+    lines = [line for line in lines if line != " "]
+
+    # filter unique lines
+    unique_lines = list(set(lines))
+    unique_lines_with_data = []
+
+    # add info for each unique line in dict format
+    for unique_line in unique_lines:
+        unique_lines_with_data.append(
+            {
+                "line": unique_line,
+                "occurence": lines.count(unique_line),
+                "len": len(unique_line),
+            }
+        )
+
+    # sort lines by length, occurence and alphabetical oder
+    sorted_unique_lines = sorted(
+        unique_lines_with_data,
+        key=lambda k: (-k["len"], -k["occurence"], k["line"].lower()),
+    )
+
+    # number lines
+    count = 1
+    for sorted_unique_line in sorted_unique_lines:
+        sorted_unique_line['line_count'] = count
+        count += 1
+
+    # save tmp file to double-check data
+    with open("ref/tmp_unique_lines.json", "w+") as out_file:
+        json.dump(sorted_unique_lines, out_file, ensure_ascii=False, indent=1)
+
+    return sorted_unique_lines
