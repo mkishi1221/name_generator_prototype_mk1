c06aa1f8b44ce8148b198ae0f271f389a1f295c6
Masayuki Kishi <masayukikishi@Masayukis-MacBook-Pro.local>
2021-04-11 22:48:30 +0900

    added randomizer to name_generator.py

--- scripts/name_generator.py
+++ scripts/name_generator.py
@@ -7,12 +7,15 @@ from modules.generate_names import generate_names
 
 def sort_data(wordlist_filepath):
 
+    #Open file
     with open(wordlist_filepath) as wordlist_file:
         words = json.load(wordlist_file)
 
     print("Generating names...")
+    # generate names
     names = generate_names(words)
 
+    # save file
     with open(sys.argv[2], "w+") as out_file:
         out_file.write(names)
 
