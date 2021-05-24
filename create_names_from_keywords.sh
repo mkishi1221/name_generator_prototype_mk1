092fd9be943a947f695db83eb53fdab3c6141a1e
Masayuki Kishi <masayukikishi@Masayukis-MacBook-Pro.local>
2021-05-24 22:15:45 +0900

    Revert "bash skip feature bug fixed"
    This reverts commit dabf24067d63f377d838782a7909d641a02d77a5.
    Push to develop instead
    
    Push to develop instead

--- create_names_from_keywords.sh
+++ create_names_from_keywords.sh
@@ -9,19 +9,19 @@ mkdir -p ref/logs
 mkdir -p results
 
 # Create log files if not exist
-touch ref/logs/keyw_prev_source_file_log.tsv
-touch ref/logs/keyw_prev_script_log.tsv
+touch ref/logs/prev_source_file_keyw_log.tsv
+touch ref/logs/prev_script_log.tsv
 
 # Reset current log files to be blank
-> ref/logs/keyw_source_file_log.tsv
-> ref/logs/keyw_script_log.tsv
+> ref/logs/source_file_keyw_log.tsv
+> ref/logs/script_log.tsv
 
 # Pour source texts modification dates into one file
 FILES=data/keywords/*.*
 for f in $FILES
 do
 ls -lh ${f} \
->> ref/logs/keyw_source_file_log.tsv
+>> ref/logs/source_file_keyw_log.tsv
 done
 
 # Pour script file modification dates into one file
@@ -29,22 +29,22 @@ FILES=scripts/*.*
 for f in $FILES
 do
 ls -lh ${f} \
->> ref/logs/keyw_script_log.tsv
+>> ref/logs/script_log.tsv
 done
 
 FILES=scripts/modules/*.*
 for f in $FILES
 do
 ls -lh ${f} \
->> ref/logs/keyw_script_log.tsv
+>> ref/logs/script_log.tsv
 done
 
 ls -lh create_names_from_keywords.sh \
->> ref/logs/keyw_script_log.tsv
+>> ref/logs/script_log.tsv
 
 # Compare contents of current and previous log files
 # If source data has changed, recompile source data, dictionary and generated name lists.
-if [[ "$(cat ref/logs/keyw_prev_source_file_log.tsv)" == "$(cat ref/logs/keyw_source_file_log.tsv)" && "$(cat ref/logs/keyw_prev_script_log.tsv)" == "$(cat ref/logs/keyw_script_log.tsv)" ]];then
+if [[ "$(cat ref/logs/prev_source_file_keyw_log.tsv)" == "$(cat ref/logs/source_file_keyw_log.tsv)" && "$(cat ref/logs/prev_script_log.tsv)" == "$(cat ref/logs/script_log.tsv)" ]];then
         echo "Source data and scripts unchanged. Moving on to domain availability check..."
 
 else
@@ -67,8 +67,8 @@ else
         tmp_keyw/alltext_keyw.tsv \
         tmp_keyw/potential_names_keyw.tsv
     
-    cat ref/logs/keyw_source_file_log.tsv > ref/logs/keyw_prev_source_file_log.tsv
-    cat ref/logs/keyw_script_log.tsv > ref/logs/keyw_prev_script_log.tsv
+    cat ref/logs/source_file_keyw_log.tsv > ref/logs/prev_source_file_keyw_log.tsv
+    cat ref/logs/script_log.tsv > ref/logs/prev_script_log.tsv
 fi
 
 # Check domains 
