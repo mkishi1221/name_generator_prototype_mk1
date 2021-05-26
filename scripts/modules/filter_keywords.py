ddc842c3187faf22945d9aafa280b264432fb919
Masayuki Kishi <masayukikishi@Masayukis-MacBook-Pro.local>
2021-05-26 23:31:20 +0900

    adjusted word filter

--- scripts/modules/filter_keywords.py
+++ scripts/modules/filter_keywords.py
@@ -8,6 +8,7 @@ def filter_keywords(keywords):
     Filter approved keywords (approved keywords may be the following):
     - Either a noun, verb, or an adjective
     - Not contain any characters except alphabets
+    - Word is at least 3 letters
     """
     approved_pos = ["noun", "verb", "adjective"]
     illegal_char = re.compile(r"[^a-zA-Z]")
@@ -15,6 +16,7 @@ def filter_keywords(keywords):
     for keyword in keywords:
         if (
             keyword.get("wordsAPI_pos") in approved_pos
+            and keyword.get("base_len") > 2
             and not bool(illegal_char.search(keyword.get("base")))
         ):
             approved_keywords.append(keyword)
