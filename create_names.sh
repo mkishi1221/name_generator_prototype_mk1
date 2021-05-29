fa2cedf59cd5be6169c5ff23421e2650a4e7db76
Masayuki Kishi <masayukikishi@Masayukis-MacBook-Pro.local>
2021-05-29 23:57:33 +0900

    keyword and sentence based generators merged

---
+++ create_names.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+# Calculate time elapsed
+date
+start_time=`gdate +%s%3N`
+
+# Create required folders
+mkdir -p tmp
+mkdir -p ref/logs
+mkdir -p results
+
+# Create log files if not exist
+touch ref/logs/prev_source_data_log.tsv
+touch ref/logs/prev_script_log.tsv
+
+# Reset current log files to be blank
+> ref/logs/source_data_log.tsv
+> ref/logs/script_log.tsv
+
+# Pour script file modification dates into one file
+FILES=scripts/*.*
+for f in $FILES
+do
+ls -lh ${f} \
+>> ref/logs/script_log.tsv
+done
+
+FILES=scripts/modules/*.*
+for f in $FILES
+do
+ls -lh ${f} \
+>> ref/logs/script_log.tsv
+done
+
+ls -lh create_names.sh \
+>> ref/logs/script_log.tsv
+
+# Check if data with sentences exists
+if [ -f data/*.txt ]; then
+    # Pour source texts modification dates into one file
+    sentences="exists"
+    FILES=data/*.*
+    for f in $FILES
+    do
+    ls -lh ${f} \
+    >> ref/logs/source_data_log.tsv
+    done
+else
+    sentences="none"
+    echo "No sentences found! Checking if keywords are supplied..."
+fi
+
+# Check if data with keywords exists
+if [ -f data/keywords/*.txt ]; then
+    # Pour source texts modification dates into one file
+    keywords="exists"
+    FILES=data/keywords/*.*
+    for f in $FILES
+    do
+    ls -lh ${f} \
+    >> ref/logs/source_data_log.tsv
+    done
+else
+    keywords="none"
+    if [ ${sentences} == "exists" ]; then
+        echo "No keywords found. Running script with only sentences..."
+    fi
+fi
+
+# Exit script if no sentences or keywords detected.
+if [ ${keywords} == "none" ] && [ ${sentences} == "none" ]; then
+    echo "No sentences and keywords detetcted! Please add source data in txt format to the \"data\" folder."
+    exit
+fi
+
+# Compare contents of current and previous log files
+# If source data has changed, recompile source data, dictionary and generated name lists.
+if [[ "$(cat ref/logs/prev_source_data_log.tsv)" == "$(cat ref/logs/source_data_log.tsv)" && "$(cat ref/logs/prev_script_log.tsv)" == "$(cat ref/logs/script_log.tsv)" ]];then
+    echo "Source data or script unchanged. Moving on to domain availability check..."
+
+else
+    echo "Source data or script changed. Recompiling source data..."
+    # Clear tmp files
+    rm -r tmp/*
+
+    # Check if data with sentences exists
+    if [ -f data/*.txt ]; then
+        # Pour source texts into one file
+        FILES=data/*.txt
+        for f in $FILES
+        do
+        cat ${f} \
+        >> tmp/alltext_sents.tsv
+        echo "" >> tmp/alltext_sents.tsv
+        done
+    else
+        > tmp/alltext_sents.tsv
+    fi
+
+    # Check if data with keywords exists
+    if [ -f data/keywords/*.txt ]; then
+        # Pour user provided keywords into one file
+        FILES=data/keywords/*.txt
+        for f in $FILES
+        do
+        cat ${f} \
+        >> tmp/alltext_keyw.tsv
+        echo "" >> tmp/alltext_keyw.tsv
+        done
+    else
+        > tmp/alltext_keyw.tsv
+    fi
+
+    # Generate word list from source text
+    # Words to be sorted by POS, length and other factors in the future to accomodate more complex name-generating algorithms.
+    echo "Creating word list..."
+    python3 scripts/keyword_generator.py \
+        tmp/alltext_sents.tsv \
+        tmp/alltext_keyw.tsv \
+        tmp/keywords.json
+
+    # Generate names
+    echo "Initiating name generator script..."
+    python3 scripts/name_generator.py \
+        tmp/keywords.json \
+        tmp/potential_names.tsv
+    
+    cat ref/logs/source_data_log.tsv > ref/logs/prev_source_data_log.tsv
+    cat ref/logs/script_log.tsv > ref/logs/prev_script_log.tsv
+fi
+
+# Check domains 
+# To make sure we don't overuse the API, the loop breaks after finding 10 available domains. You can change this limit by changing the "limit" variable defined below.
+echo "Choosing names and initiating domain availability check..."
+echo ""
+dt=$(gdate '+%Y%m%d_%H%M%S')
+limit=10
+python3 scripts/domain_checker.py \
+    tmp/potential_names.tsv \
+    results/names_${dt}.tsv \
+    ${limit}
+
+# Calculate time elapsed
+end_time=`gdate +%s%3N`
+min_elapsed=$(echo "scale=0; (${end_time}-${start_time})/1000/60" | bc )
+sec_elapsed=$(echo "scale=3; ((${end_time}-${start_time})/1000)-(${min_elapsed}*60)" | bc )
+echo "All files processed. Total: ${min_elapsed}min, ${sec_elapsed}sec." 
+date
--- scripts/modules/filter_keywords.py
+++ scripts/modules/filter_keywords.py
@@ -15,16 +15,20 @@ def filter_keywords(keywords):
     approved_keywords = []
 
     for keyword in keywords:
+
+        # Remove unused keyword dict keys
         tmp_keyword = keyword.copy()
         tmp_keyword.pop('word')
-        tmp_keyword.pop('lemma')
-        tmp_keyword.pop('spacy_pos')
-        tmp_keyword.pop('occurence')
+        tmp_keyword.pop('lemma', None)
+        tmp_keyword.pop('spacy_pos', None)
+        tmp_keyword.pop('occurence', None)
+
+        # Filter approved keywords
         if (
             tmp_keyword.get("wordsAPI_pos") in approved_pos
             and tmp_keyword.get("base_len") > 2
             and not bool(illegal_char.search(tmp_keyword.get("base")))
-            and tmp_keyword not in approved_keywords
+            and not any(tmp_keyword['base'] == d['base'] and tmp_keyword['wordsAPI_pos'] == d['wordsAPI_pos'] for d in approved_keywords)
         ):
             approved_keywords.append(tmp_keyword)
 
