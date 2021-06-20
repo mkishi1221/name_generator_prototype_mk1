d45ef3c2de90b19bbdedaa515ae05fc6545399e9
Masayuki Kishi <masayukikishi@Masayukis-MacBook-Pro.local>
2021-06-20 12:49:23 +0900

    adjustments to keyword blacklist output format

--- blwl_generator.py
+++ blwl_generator.py
@@ -43,16 +43,24 @@ def create_keyword_blacklist(file):
     # Get all keywords (in keyword1 column) that have been marked "b" (for "blacklisted")
     keyword_blacklist_df1 = df.loc[df['Keyword 1 check'] == 'b']
     # Remove unwanted columns
-    keyword_blacklist_df1.drop(['Name and Domain check', 'Keyword 1 check', 'keyword2', 'Keyword 2 check'], axis=1, inplace=True)
+    keyword_blacklist_df1.drop(
+        ['Name and Domain check', 'Keyword 1 check', 'keyword2', 'Keyword 2 check', 'length', 'domain', 'all_keywords'], 
+        axis=1,
+        inplace=True
+    )
     # Rename keyword1 column as 'keyword'
     keyword_blacklist_df1['keyword'] = keyword_blacklist_df1.pop('keyword1')
     # Get pos data from algorithm (For keyword 1, its first word in the algorithm column) and add to new pos column
-    keyword_blacklist_df1['pos'] = keyword_blacklist_df1['algorithm'].map(lambda v: v.split(' + ')[0])
+    keyword_blacklist_df1['wordsAPI_pos'] = keyword_blacklist_df1['algorithm'].map(lambda v: v.split(' + ')[0])
 
     # Get all keywords (in keyword2 column) that have been marked "b" (for "blacklisted")
     keyword_blacklist_df2 = df.loc[df['Keyword 2 check'] == 'b']
     # Remove unwanted columns
-    keyword_blacklist_df2.drop(['Name and Domain check', 'Keyword 1 check', 'keyword1', 'Keyword 2 check'], axis=1, inplace=True)
+    keyword_blacklist_df2.drop(
+        ['Name and Domain check', 'Keyword 1 check', 'keyword1', 'Keyword 2 check', 'length', 'domain', 'all_keywords'],
+        axis=1,
+        inplace=True
+    )
     # Rename keyword2 column as 'keyword'
     keyword_blacklist_df2['keyword'] = keyword_blacklist_df2.pop('keyword2')
     # Get pos data from algorithm (For keyword 1, its first word in the algorithm column) and add to new pos column
@@ -61,6 +69,8 @@ def create_keyword_blacklist(file):
     # Combine both keyword1 and keyword2 df
     keyword_blacklist_df = pd.concat([keyword_blacklist_df1, keyword_blacklist_df2])
     keyword_blacklist_df.fillna('', inplace=True)
+    keyword_blacklist_df['keyword_len'] = keyword_blacklist_df['keyword'].str.len()
+    keyword_blacklist_df = keyword_blacklist_df[['keyword_len', 'keyword', 'wordsAPI_pos', 'algorithm', 'name']]
     tmp_keyword_blacklist = keyword_blacklist_df.to_dict('records')
 
     # Load pre-exisiting keyword blacklist file if exists and add data. If not exists, make new blacklist file
